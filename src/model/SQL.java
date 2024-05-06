bec88fc0d46554de5e299f65e7341c0e3477e96a
Ashan-Javainstitute <ashan.javainstitute@gmail.com>
2024-05-06 12:47:44 +0530

    First init
    First init

---
+++ src/model/SQL.java
@@ -0,0 +1,40 @@
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class SQL {
+
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/textile_db", "root", "1515151515Gmail@@@@");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ResultSet execute(String query) {
+
+        try {
+
+            Statement statement = connection.createStatement();
+
+            if (query.startsWith("SELECT")) {
+                ResultSet resultSet = statement.executeQuery(query);
+                return resultSet;
+            } else {
+                int result = statement.executeUpdate(query);
+                return null;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
